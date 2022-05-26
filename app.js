d25f2fbc0b9b36adbd551a52ee7fef26d274682b
smzahurul <zahurul.it@gmail.com>
2022-05-26 19:33:41 +0600

    Rest Country

---
+++ app.js
@@ -0,0 +1,47 @@
+fetch('https://restcountries.com/v2/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+
+const displayCountries = countries => {
+    const countriesDiv = document.getElementById('countries-name');
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div');
+        countryDiv.className = "country"
+
+        const countryInfo = `
+        <h2 class="country-name">${country.name}</h2>
+        <p>${country.capital}</p>
+        <p>${country.region}</p>
+        <p>${country.population}</p>
+        <p>${country.latlng}</p>
+        `
+        countryDiv.innerHTML = countryInfo;
+        countriesDiv.appendChild(countryDiv);
+    });
+    // 
+    //     const countryDiv = document.createElement('div');
+
+    //     // const h3 = document.createElement('h3');
+    //     // h3.innerText = country.name;
+    //     // const p = document.createElement('p');
+    //     // p.innerText = country.capital;
+    //     // countryDiv.appendChild(h3);
+    //     // countryDiv.appendChild(p);
+
+    //     countryDiv.className = "country"
+
+    //     const countryInfo = `
+    //     <h2 class="country-name">${country.name}</h2>
+    //     <p>${country.capital}</p>
+    //     <p>${country.region}</p>
+    //     <p>${country.population}</p>
+    //     <p>${country.latlng}</p>
+    //     `
+    //     countryDiv.innerHTML = countryInfo;
+    //     countriesDiv.appendChild(countryDiv);
+
+
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+    // }
+}
